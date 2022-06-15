c13d49c7c6ae50ac47e536e190a2710e14f078fd
mervatgithub <mervat.github@gmail.com>
2022-06-15 18:13:04 +0200

    clean up comments

--- lib/providers/weather_provider.dart
+++ lib/providers/weather_provider.dart
@@ -2,22 +2,6 @@
 import 'package:flutter/material.dart';
 
 import '../models/weather_model.dart';
-//
-// class WeatherProvider extends ChangeNotifier{
-//
-//   WeatherModel? _weatherData;
-//
-//   set _weatherData (WeatherModel? weather){
-//     _weatherData = weather;
-//     notifyListeners();
-//   }
-//
-//   WeatherModel? get weatherData => _weatherData!;
-//
-//
-//
-//
-// }
 
 class WeatherProvider extends ChangeNotifier {
   WeatherModel? _weatherData;
